d4c2f91bef00645c3ca66ac9ad7b025a61de085a
GokulKrishnan10 <gokulkrish.elangovan10@gmail.com>
2024-02-11 22:28:15 +0530

    Made changes

--- test/java/com/livedocs/server/webapp/WebappApplicationTests.java
+++ test/java/com/livedocs/server/webapp/WebappApplicationTests.java
@@ -8,7 +8,7 @@ class WebappApplicationTests {
 
 	@Test
 	void contextLoads() {
-		System.out.println("Changes observed at test/ directory but not working");
+		System.out.println("Changes observed at test/ directory");
 	}
 
 }
