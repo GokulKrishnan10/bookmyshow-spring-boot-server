adc81aa21a6f32133ae85f9011fa31a611b5e120
GokulKrishnan10 <gokulkrish.elangovan10@gmail.com>
2024-02-11 22:56:00 +0530

    Made changes to app

--- test/java/com/livedocs/server/webapp/WebappApplicationTests.java
+++ test/java/com/livedocs/server/webapp/WebappApplicationTests.java
@@ -8,7 +8,7 @@ class WebappApplicationTests {
 
 	@Test
 	void contextLoads() {
-		System.out.println("Changes observed at test/ directory but");
+		System.out.println("Changes observed at test/ directory");
 	}
 
 }
