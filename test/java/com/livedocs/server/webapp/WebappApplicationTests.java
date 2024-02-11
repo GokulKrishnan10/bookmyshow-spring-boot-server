cc8e3a86b84502e52b3b0eccf3b135ec896588ae
GokulKrishnan10 <gokulkrish.elangovan10@gmail.com>
2024-02-11 23:29:31 +0530

    Made changes to app

--- test/java/com/livedocs/server/webapp/WebappApplicationTests.java
+++ test/java/com/livedocs/server/webapp/WebappApplicationTests.java
@@ -8,7 +8,7 @@ class WebappApplicationTests {
 
 	@Test
 	void contextLoads() {
-		System.out.println("Changes observed at test/ but not");
+		System.out.println("Changes observed at test/ but not working");
 	}
 
 }
